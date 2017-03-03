350368cee2f2372f2b17df15a6aa0242067490ba
Daniel194 <lungu.daniel94@gmail.com>
2017-03-04 00:03:28 +0200

    Load Sunnybrook - Done

--- Code/load_sunnybrook.py
+++ Code/load_sunnybrook.py
@@ -7,51 +7,12 @@ import warnings
 warnings.filterwarnings('ignore')  # we ignore a RuntimeWarning produced from dividing by zero
 np.random.seed(1234)
 
-SAX_SERIES = {
-    # challenge training
-    "SC-HF-I-1": "0004",
-    "SC-HF-I-2": "0106",
-    "SC-HF-I-4": "0116",
-    "SC-HF-I-40": "0134",
-    "SC-HF-NI-3": "0379",
-    "SC-HF-NI-4": "0501",
-    "SC-HF-NI-34": "0446",
-    "SC-HF-NI-36": "0474",
-    "SC-HYP-1": "0550",
-    "SC-HYP-3": "0650",
-    "SC-HYP-38": "0734",
-    "SC-HYP-40": "0755",
-    "SC-N-2": "0898",
-    "SC-N-3": "0915",
-    "SC-N-40": "0944",
-}
-SUNNYBROOK_ROOT_PATH = "data/"
-TRAIN_CONTOUR_PATH = os.path.join(SUNNYBROOK_ROOT_PATH, "Sunnybrook Cardiac MR Database ContoursPart3",
-                                  "TrainingDataContours")
-TRAIN_IMG_PATH = os.path.join(SUNNYBROOK_ROOT_PATH, "challenge_training")
-
-BATCHSIZE = 100
-NR_EPOCHS = 5
-
-
-def shrink_case(case):
-    toks = case.split("-")
-
-    def shrink_if_number(x):
-        try:
-            cvt = int(x)
-            return str(cvt)
-        except ValueError:
-            return x
-
-    return "-".join([shrink_if_number(t) for t in toks])
-
 
 class Contour(object):
     def __init__(self, ctr_path):
         self.ctr_path = ctr_path
         match = re.search(r"/([^/]*)/contours-manual/IRCCI-expert/IM-0001-(\d{4})-icontour-manual.txt", ctr_path)
-        self.case = shrink_case(match.group(1))
+        self.case = self.__shrink_case(match.group(1))
         self.img_no = int(match.group(2))
 
     def __str__(self):
@@ -59,8 +20,29 @@ class Contour(object):
 
     __repr__ = __str__
 
+    def __shrink_case(self, case):
+        toks = case.split("-")
+
+        def shrink_if_number(x):
+            try:
+                cvt = int(x)
+                return str(cvt)
+            except ValueError:
+                return x
+
+        return "-".join([shrink_if_number(t) for t in toks])
+
 
 def load_contour(contour, img_path):
+    """
+    Function maps each contour file to a DICOM image, according to a case study number,
+    using the SAX_SERIES dictionary and the extracted image number.
+    Upon successful mapping, the function returns NumPy arrays for a pair of DICOM image and contour file, or label.
+    :param contour: and array which contains all contour path
+    :param img_path: path to image folder
+    :return: return images and label
+    """
+
     filename = "IM-%s-%04d.dcm" % (SAX_SERIES[contour.case], contour.img_no)
     full_path = os.path.join(img_path, contour.case, filename)
     f = dicom.read_file(full_path)
@@ -73,6 +55,13 @@ def load_contour(contour, img_path):
 
 
 def get_all_contours(contour_path):
+    """
+    Function walks through a directory containing contour files,
+    and extracts the necessary case study number and image number from a contour filename using the Contour class.
+    :param contour_path: path to contour file
+    :return: extracted path
+    """
+
     contours = [os.path.join(dirpath, f)
                 for dirpath, dirnames, files in os.walk(contour_path)
                 for f in fnmatch.filter(files, 'IM-0001-*-icontour-manual.txt')]
@@ -86,6 +75,13 @@ def get_all_contours(contour_path):
 
 
 def export_all_contours(batch, img_path):
+    """
+    Function return an array with all images and labels for a specific batch.
+    :param batch: an array with all path to contour file.
+    :param img_path: image path to all images
+    :return: return two arrays, one for images and one for labels
+    """
+
     imgs, labels = [], []
 
     if len(batch) == 0:
@@ -112,6 +108,31 @@ def export_all_contours(batch, img_path):
 
 if __name__ == "__main__":
     SPLIT_RATIO = 0.1
+    SAX_SERIES = {
+        # challenge training
+        "SC-HF-I-1": "0004",
+        "SC-HF-I-2": "0106",
+        "SC-HF-I-4": "0116",
+        "SC-HF-I-40": "0134",
+        "SC-HF-NI-3": "0379",
+        "SC-HF-NI-4": "0501",
+        "SC-HF-NI-34": "0446",
+        "SC-HF-NI-36": "0474",
+        "SC-HYP-1": "0550",
+        "SC-HYP-3": "0650",
+        "SC-HYP-38": "0734",
+        "SC-HYP-40": "0755",
+        "SC-N-2": "0898",
+        "SC-N-3": "0915",
+        "SC-N-40": "0944",
+    }
+    SUNNYBROOK_ROOT_PATH = "data/"
+    TRAIN_CONTOUR_PATH = os.path.join(SUNNYBROOK_ROOT_PATH, "Sunnybrook Cardiac MR Database ContoursPart3",
+                                      "TrainingDataContours")
+    TRAIN_IMG_PATH = os.path.join(SUNNYBROOK_ROOT_PATH, "challenge_training")
+
+    BATCHSIZE = 100
+    NR_EPOCHS = 5
 
     print("Mapping ground truth contours to images...")
 
