be819f325395f4f40c61da866e6a7459eaedf395
Daniel194 <lungu.daniel94@gmail.com>
2017-03-12 22:51:48 +0200

    Test

--- Code/test.py
+++ Code/test.py
@@ -1,32 +1,22 @@
 import os
 import random
 import tensorflow as tf
-# import wget
-import tarfile
-import numpy as np
-import argparse
-
 import time
-from datetime import datetime
-
-from utils import input_pipeline
-from tensorflow.python.framework import ops
-from tensorflow.python.ops import gen_nn_ops
+import numpy as np
+import cv2
 
 
 class DeconvNet:
-    def __init__(self, images, segmentations, use_cpu=False, checkpoint_dir='./checkpoints/'):
-        # self.maybe_download_and_extract()
+    def __init__(self, use_cpu=False, checkpoint_dir='./checkpoints/'):
+        self.maybe_download_and_extract()
 
-        self.x = images
-        self.y = segmentations
         self.build(use_cpu=use_cpu)
 
-        # self.saver = tf.train.Saver(max_to_keep = 5, keep_checkpoint_every_n_hours =1)
-        self.saver = tf.train.Saver(tf.global_variables(), max_to_keep=5, keep_checkpoint_every_n_hours=1)  # v0.12
+        self.saver = tf.train.Saver(max_to_keep = 5, keep_checkpoint_every_n_hours =1)
+        config = tf.ConfigProto(allow_soft_placement = True)
+        self.session = tf.Session(config = config)
+        self.session.run(tf.initialize_all_variables())
         self.checkpoint_dir = checkpoint_dir
-        # self.rate=lr
-        # start=time.time()
 
     def restore_session(self):
         global_step = 0
@@ -42,16 +32,41 @@ class DeconvNet:
 
         return global_step
 
-        # ! Does not work for now ! But currently I'm working on it -> PR's welcome!
 
     def predict(self, image):
         self.restore_session()
         return self.prediction.eval(session=self.session, feed_dict={image: [image]})[0]
 
 
-    @ops.RegisterGradient("MaxPoolWithArgmax")
-    def _MaxPoolGradWithArgmax(op, grad, unused_argmax_grad):
-        return gen_nn_ops._max_pool_grad_with_argmax(op.inputs[0], grad, op.outputs[1], op.get_attr("ksize"), op.get_attr("strides"), padding=op.get_attr("padding"))
+    def train(self, train_stage=1, training_steps=1000, restore_session=False, learning_rate=1e-6):
+        if restore_session:
+            step_start = restore_session()
+        else:
+            step_start = 0
+
+        if train_stage == 1:
+            trainset = open('data/stage_1_train_imgset/train.txt').readlines()
+        else:
+            trainset = open('data/stage_2_train_imgset/train.txt').readlines()
+
+        for i in range(step_start, step_start+training_steps):
+            # pick random line from file
+            random_line = random.choice(trainset)
+            image_file = random_line.split(' ')[0]
+            ground_truth_file = random_line.split(' ')[1]
+            image = np.float32(cv2.imread('data' + image_file))
+            ground_truth = cv2.imread('data' + ground_truth_file[:-1], cv2.IMREAD_GRAYSCALE)
+            # norm to 21 classes [0-20] (see paper)
+            ground_truth = (ground_truth / 255) * 20
+            print('run train step: '+str(i))
+            start = time.time()
+            self.train_step.run(session=self.session, feed_dict={self.x: [image], self.y: [ground_truth], self.rate: learning_rate})
+
+            if i % 10000 == 0:
+                print('step {} finished in {:.2f} s with loss of {:.6f}'.format(
+                    i, time.time() - start, self.loss.eval(session=self.session, feed_dict={self.x: [image], self.y: [ground_truth]})))
+                self.saver.save(self.session, self.checkpoint_dir+'model', global_step=i)
+                print('Model {} saved'.format(i))
 
     def build(self, use_cpu=False):
         '''
@@ -67,6 +82,10 @@ class DeconvNet:
             device = '/gpu:0'
 
         with tf.device(device):
+            self.x = tf.placeholder(tf.float32, shape=(1, None, None, 3))
+            self.y = tf.placeholder(tf.int64, shape=(1, None, None))
+            expected = tf.expand_dims(self.y, -1)
+            self.rate = tf.placeholder(tf.float32, shape=[])
 
             conv_1_1 = self.conv_layer(self.x, [3, 3, 3, 64], 64, 'conv_1_1')
             conv_1_2 = self.conv_layer(conv_1_1, [3, 3, 64, 64], 64, 'conv_1_2')
@@ -101,42 +120,44 @@ class DeconvNet:
 
             deconv_fc_6 = self.deconv_layer(fc_7, [7, 7, 512, 4096], 512, 'fc6_deconv')
 
-            # unpool_5 = self.unpool_layer2x2_batch(deconv_fc_6, pool_5_argmax, tf.shape(conv_5_3))
-            unpool_5 = self.unpool_layer2x2_batch(deconv_fc_6, pool_5_argmax)
+            unpool_5 = self.unpool_layer2x2(deconv_fc_6, pool_5_argmax, tf.shape(conv_5_3))
 
             deconv_5_3 = self.deconv_layer(unpool_5, [3, 3, 512, 512], 512, 'deconv_5_3')
             deconv_5_2 = self.deconv_layer(deconv_5_3, [3, 3, 512, 512], 512, 'deconv_5_2')
             deconv_5_1 = self.deconv_layer(deconv_5_2, [3, 3, 512, 512], 512, 'deconv_5_1')
 
-            # unpool_4 = self.unpool_layer2x2_batch(deconv_5_1, pool_4_argmax, tf.shape(conv_4_3))
-            unpool_4 = self.unpool_layer2x2_batch(deconv_5_1, pool_4_argmax)
+            unpool_4 = self.unpool_layer2x2(deconv_5_1, pool_4_argmax, tf.shape(conv_4_3))
 
             deconv_4_3 = self.deconv_layer(unpool_4, [3, 3, 512, 512], 512, 'deconv_4_3')
             deconv_4_2 = self.deconv_layer(deconv_4_3, [3, 3, 512, 512], 512, 'deconv_4_2')
             deconv_4_1 = self.deconv_layer(deconv_4_2, [3, 3, 256, 512], 256, 'deconv_4_1')
 
-            # unpool_3 = self.unpool_layer2x2_batch(deconv_4_1, pool_3_argmax, tf.shape(conv_3_3))
-            unpool_3 = self.unpool_layer2x2_batch(deconv_4_1, pool_3_argmax)
+            unpool_3 = self.unpool_layer2x2(deconv_4_1, pool_3_argmax, tf.shape(conv_3_3))
 
             deconv_3_3 = self.deconv_layer(unpool_3, [3, 3, 256, 256], 256, 'deconv_3_3')
             deconv_3_2 = self.deconv_layer(deconv_3_3, [3, 3, 256, 256], 256, 'deconv_3_2')
             deconv_3_1 = self.deconv_layer(deconv_3_2, [3, 3, 128, 256], 128, 'deconv_3_1')
 
-            # unpool_2 = self.unpool_layer2x2_batch(deconv_3_1, pool_2_argmax, tf.shape(conv_2_2))
-            unpool_2 = self.unpool_layer2x2_batch(deconv_3_1, pool_2_argmax)
+            unpool_2 = self.unpool_layer2x2(deconv_3_1, pool_2_argmax, tf.shape(conv_2_2))
 
             deconv_2_2 = self.deconv_layer(unpool_2, [3, 3, 128, 128], 128, 'deconv_2_2')
             deconv_2_1 = self.deconv_layer(deconv_2_2, [3, 3, 64, 128], 64, 'deconv_2_1')
 
-            # unpool_1 = self.unpool_layer2x2_batch(deconv_2_1, pool_1_argmax, tf.shape(conv_1_2))
-            unpool_1 = self.unpool_layer2x2_batch(deconv_2_1, pool_1_argmax)
+            unpool_1 = self.unpool_layer2x2(deconv_2_1, pool_1_argmax, tf.shape(conv_1_2))
 
             deconv_1_2 = self.deconv_layer(unpool_1, [3, 3, 64, 64], 64, 'deconv_1_2')
             deconv_1_1 = self.deconv_layer(deconv_1_2, [3, 3, 32, 64], 32, 'deconv_1_1')
 
-            score_1 = self.deconv_layer(deconv_1_1, [1, 1, 21, 32], 21, 'score_1')
+            score_1 = self.deconv_layer(deconv_1_1, [1, 1, 2, 32], 2, 'score_1')
 
-            self.logits = tf.reshape(score_1, (-1, 21))
+            logits = tf.reshape(score_1, (-1, 2))
+            cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits, tf.reshape(expected, [-1]), name='x_entropy')
+            self.loss = tf.reduce_mean(cross_entropy, name='x_entropy_mean')
+
+            self.train_step = tf.train.AdamOptimizer(self.rate).minimize(self.loss)
+
+            self.prediction = tf.argmax(tf.reshape(tf.nn.softmax(logits), tf.shape(score_1)), dimension=3)
+            self.accuracy = tf.reduce_sum(tf.pow(self.prediction - expected, 2))
 
     def weight_variable(self, shape):
         initial = tf.truncated_normal(shape, stddev=0.1)
@@ -152,11 +173,9 @@ class DeconvNet:
         return tf.nn.relu(tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding=padding) + b)
 
     def pool_layer(self, x):
-        '''
-        see description of build method
-        '''
         with tf.device('/gpu:0'):
             return tf.nn.max_pool_with_argmax(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
 
     def deconv_layer(self, x, W_shape, b_shape, name, padding='SAME'):
         W = self.weight_variable(W_shape)
@@ -171,11 +190,9 @@ class DeconvNet:
         output_list = []
         output_list.append(argmax // (shape[2] * shape[3]))
         output_list.append(argmax % (shape[2] * shape[3]) // shape[3])
-
         return tf.pack(output_list)
 
     def unpool_layer2x2(self, x, raveled_argmax, out_shape):
-
         argmax = self.unravel_argmax(raveled_argmax, tf.to_int64(out_shape))
         output = tf.zeros([out_shape[1], out_shape[2], out_shape[3]])
 
@@ -202,114 +219,53 @@ class DeconvNet:
         values = tf.reshape(x1, [-1])
 
         delta = tf.SparseTensor(indices, values, tf.to_int64(tf.shape(output)))
-
         return tf.expand_dims(tf.sparse_tensor_to_dense(tf.sparse_reorder(delta)), 0)
 
-    def unpool_layer2x2_batch(self, bottom, argmax):
-        bottom_shape = tf.shape(bottom)
-        top_shape = [bottom_shape[0], bottom_shape[1] * 2, bottom_shape[2] * 2, bottom_shape[3]]
+    def unpool_layer2x2_batch(self, x, argmax):
+        '''
+        Args:
+            x: 4D tensor of shape [batch_size x height x width x channels]
+            argmax: A Tensor of type Targmax. 4-D. The flattened indices of the max
+            values chosen for each output.
+        Return:
+            4D output tensor of shape [batch_size x 2*height x 2*width x channels]
+        '''
+        x_shape = tf.shape(x)
+        out_shape = [x_shape[0], x_shape[1]*2, x_shape[2]*2, x_shape[3]]
 
-        batch_size = top_shape[0]
-        height = top_shape[1]
-        width = top_shape[2]
-        channels = top_shape[3]
+        batch_size = out_shape[0]
+        height = out_shape[1]
+        width = out_shape[2]
+        channels = out_shape[3]
 
         argmax_shape = tf.to_int64([batch_size, height, width, channels])
         argmax = self.unravel_argmax(argmax, argmax_shape)
 
         t1 = tf.to_int64(tf.range(channels))
-        t1 = tf.tile(t1, [batch_size * (width // 2) * (height // 2)])
+        t1 = tf.tile(t1, [batch_size*(width//2)*(height//2)])
         t1 = tf.reshape(t1, [-1, channels])
         t1 = tf.transpose(t1, perm=[1, 0])
-        t1 = tf.reshape(t1, [channels, batch_size, height // 2, width // 2, 1])
+        t1 = tf.reshape(t1, [channels, batch_size, height//2, width//2, 1])
         t1 = tf.transpose(t1, perm=[1, 0, 2, 3, 4])
 
         t2 = tf.to_int64(tf.range(batch_size))
-        t2 = tf.tile(t2, [channels * (width // 2) * (height // 2)])
+        t2 = tf.tile(t2, [channels*(width//2)*(height//2)])
         t2 = tf.reshape(t2, [-1, batch_size])
         t2 = tf.transpose(t2, perm=[1, 0])
-        t2 = tf.reshape(t2, [batch_size, channels, height // 2, width // 2, 1])
+        t2 = tf.reshape(t2, [batch_size, channels, height//2, width//2, 1])
 
         t3 = tf.transpose(argmax, perm=[1, 4, 2, 3, 0])
 
         t = tf.concat(4, [t2, t3, t1])
-        indices = tf.reshape(t, [(height // 2) * (width // 2) * channels * batch_size, 4])
+        indices = tf.reshape(t, [(height//2)*(width//2)*channels*batch_size, 4])
 
-        x1 = tf.transpose(bottom, perm=[0, 3, 1, 2])
+        x1 = tf.transpose(x, perm=[0, 3, 1, 2])
         values = tf.reshape(x1, [-1])
 
-        return tf.scatter_nd(indices, values, tf.to_int64(top_shape))
+        delta = tf.SparseTensor(indices, values, tf.to_int64(out_shape))
+        return tf.sparse_tensor_to_dense(tf.sparse_reorder(delta))
 
 
 if __name__ == '__main__':
-
-    # Using argparse over tf.FLAGS as I find they behave better in ipython
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--train_record', help="training tfrecord file", default="tfrecords/pascalvoc2012.tfrecords")
-    parser.add_argument('--train_dir', help="where to log training", default="train_log")
-    parser.add_argument('--batch_size', help="batch size", type=int, default=10)
-    parser.add_argument('--num_epochs', help="number of epochs.", type=int, default=50)
-    parser.add_argument('--lr', help="learning rate", type=float, default=1e-6)
-    args = parser.parse_args()
-
-    trn_images_batch, trn_segmentations_batch = input_pipeline(args.train_record, args.batch_size, args.num_epochs)
-
-    deconvnet = DeconvNet(trn_images_batch, trn_segmentations_batch, use_cpu=False)
-
-    logits = deconvnet.logits
-
-    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits, tf.cast(tf.reshape(deconvnet.y, [-1]), tf.int64), name='x_entropy')
-
-    loss_mean = tf.reduce_mean(cross_entropy, name='x_entropy_mean')
-
-    train_step = tf.train.AdamOptimizer(args.lr).minimize(loss_mean)
-
-    summary_op = tf.summary.merge_all()  # v0.12
-
-    # init = tf.initialize_all_variables()
-    # init_locals = tf.initialize_local_variables()
-
-    init_global = tf.global_variables_initializer()  # v0.12
-    init_locals = tf.local_variables_initializer()  # v0.12
-
-    config = tf.ConfigProto(allow_soft_placement=True)
-
-    with tf.Session(config=config) as sess:
-
-        sess.run([init_global, init_locals])
-
-        coord = tf.train.Coordinator()
-        threads = tf.train.start_queue_runners(coord=coord)
-
-        # summary_writer = tf.train.SummaryWriter(args.train_dir, sess.graph)
-        summary_writer = tf.summary.FileWriter(args.train_dir, sess.graph)  # v0.12
-        # training_summary = tf.scalar_summary("loss", loss_mean)
-        training_summary = tf.summary.scalar("loss", loss_mean)  # v0.12
-
-        try:
-            step = 0
-            while not coord.should_stop():
-                start_time = time.time()
-                _, loss_val, train_sum = sess.run([train_step, loss_mean, training_summary])
-                elapsed = time.time() - start_time
-                summary_writer.add_summary(train_sum, step)
-                # print sess.run(deconvnet.prediction)
-
-                assert not np.isnan(loss_val), 'Model diverged with loss = NaN'
-
-                step += 1
-
-                if step % 1 == 0:
-                    num_examples_per_step = args.batch_size
-                    examples_per_sec = num_examples_per_step / elapsed
-                    sec_per_batch = float(elapsed)
-
-                    format_str = ('%s: step %d, loss = %.2f (%.1f examples/sec; %.3f sec/batch)')
-                    print(format_str % (datetime.now(), step, loss_val, examples_per_sec, sec_per_batch))
-
-
-        except tf.errors.OutOfRangeError:
-            print('Done training -- epoch limit reached')
-        finally:
-            coord.request_stop()
-            coord.join(threads)
+    deconvNet = DeconvNet()
+    deconvNet.train()
