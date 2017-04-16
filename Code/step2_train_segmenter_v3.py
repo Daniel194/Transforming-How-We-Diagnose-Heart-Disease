e0612e7524835aee38d5c2ced48ff09f03a748aa
Daniel194 <lungu.daniel94@gmail.com>
2017-04-16 19:58:00 +0300

    Segmenter 3 - U-Net - WIP

--- Code/step2_train_segmenter_v3.py
+++ Code/step2_train_segmenter_v3.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import random
+import pickle
 import math
 import tensorflow as tf
 import numpy as np
@@ -11,64 +12,48 @@ import utils.sunnybrook as sunnybrook
 
 
 class LVSegmentation(object):
-    def __init__(self, learning_rate, checkpoint_dir='../../result/segmenter/train_result/v3/'):
-        self.x = tf.placeholder(tf.float32, shape=(None, 224, 224, 1))
-        self.y = tf.placeholder(tf.int64, shape=(None, 224, 224))
-        self.keep_prob = tf.placeholder(tf.float32)
+    def __init__(self, checkpoint_dir='../../result/segmenter/train_result/v3/'):
+        self.__create_conv_net()
+        self.__saver = tf.train.Saver(max_to_keep=30, keep_checkpoint_every_n_hours=1)
+        self.__session = tf.Session()
+        self.__session.run(tf.global_variables_initializer())
 
-        logits = self.__create_conv_net()
-        self.cost = self.__cost_function(logits)
-        self.optimizer = self.__adam_optimizer(learning_rate)
-        self.prediction = self.__predict_function(logits)
-
-        self.saver = tf.train.Saver(max_to_keep=30, keep_checkpoint_every_n_hours=1)
-        self.session = tf.Session()
-        self.session.run(tf.global_variables_initializer())
-
-        self.checkpoint_dir = checkpoint_dir
+        self.__checkpoint_dir = checkpoint_dir
+        self.__loss_array = []
 
     def predict(self, images):
         self.__restore_session()
 
-        return self.prediction.eval(session=self.session, feed_dict={self.x: images, self.keep_prob: 1.})
+        return self.__prediction.eval(session=self.__session, feed_dict={self.x: images, self.keep_prob: 1.0})
 
-    def train(self, train_paths, train_size, batch_size, epochs=30, dropout=0.75, restore_session=False):
-
+    def train(self, train_paths, epochs=30, batch_size=2, restore_session=False, learning_rate=1e-6, dropout=0.75):
         if restore_session:
             self.__restore_session()
 
-        avg_gradients = None
+        train_size = len(train_paths)
 
         for epoch in range(epochs):
             total_loss = 0
 
             for step in range(0, train_size, batch_size):
-                current_step = train_size * epoch + step
-
                 train_path = train_paths[step:step + batch_size]
                 _, images, labels = self.read_data(train_path)
 
-                _, loss, lr, gradients = self.session.run(
-                    (self.optimizer, self.cost, self.learning_rate_node, self.gradients_node),
-                    feed_dict={self.x: images,
-                               self.y: labels,
-                               self.keep_prob: dropout})
+                self.__train_step.run(session=self.__session,
+                                      feed_dict={self.x: images, self.y: labels, self.rate: learning_rate,
+                                                 self.keep_prob: dropout})
 
-                if avg_gradients is None:
-                    avg_gradients = [np.zeros_like(gradient) for gradient in gradients]
-
-                for i in range(len(gradients)):
-                    avg_gradients[i] = (avg_gradients[i] * (1.0 - (1.0 / (current_step + 1)))) + (
-                        gradients[i] / (current_step + 1))
-
-                norm_gradients = [np.linalg.norm(gradient) for gradient in avg_gradients]
-                self.norm_gradients_node.assign(norm_gradients).eval(session=self.session)
+                loss = self.__loss.eval(session=self.__session,
+                                        feed_dict={self.x: images, self.y: labels, self.keep_prob: 1.0})
 
                 total_loss += loss
 
-            self.__output_epoch_stats(epoch, total_loss, train_size)
+            print('Epoch {} - Loss : {:.6f}'.format(epoch, total_loss / train_size))
 
-            self.__save(epoch)
+            self.__saver.save(self.__session, self.__checkpoint_dir + 'model', global_step=epoch)
+
+            self.__loss_array.append(total_loss / train_size)
+            self.__save_loss()
 
     def read_data(self, paths):
         images, labels = sunnybrook.export_all_contours(paths)
@@ -91,14 +76,14 @@ class LVSegmentation(object):
 
     def __create_conv_net(self, layers=5, features_root=32, filter_size=3, pool_size=2):
 
-        pools = OrderedDict()
-        deconv = OrderedDict()
+        self.x = tf.placeholder(tf.float32, shape=(None, 224, 224, 1))
+        self.y = tf.placeholder(tf.int64, shape=(None, 224, 224))
+        self.keep_prob = tf.placeholder(tf.float32)
+        self.rate = tf.placeholder(tf.float32, shape=[])
+
+        expected = tf.expand_dims(self.y, -1)
+
         dw_h_convs = OrderedDict()
-        up_h_convs = OrderedDict()
-
-        in_size = 1000
-        size = in_size
-
         in_node = self.x
 
         # down layers
@@ -112,19 +97,16 @@ class LVSegmentation(object):
 
             w2 = self.__weight_variable([filter_size, filter_size, features, features])
 
-            b1 = self.__variable([features], 0.0)
-            b2 = self.__variable([features], 0.0)
+            b1 = self.__variable([features], 0.1)
+            b2 = self.__variable([features], 0.1)
 
             conv1 = self.__conv_layer(in_node, w1, self.keep_prob)
             tmp_h_conv = tf.nn.relu(conv1 + b1)
             conv2 = self.__conv_layer(tmp_h_conv, w2, self.keep_prob)
             dw_h_convs[layer] = tf.nn.relu(conv2 + b2)
 
-            size -= 4
             if layer < layers - 1:
-                pools[layer] = self.__max_pool(dw_h_convs[layer], pool_size)
-                in_node = pools[layer]
-                size /= 2
+                in_node = self.__max_pool(dw_h_convs[layer], pool_size)
 
         in_node = dw_h_convs[layers - 1]
 
@@ -133,56 +115,37 @@ class LVSegmentation(object):
             features = 2 ** (layer + 1) * features_root
 
             wd = self.__weight_variable([pool_size, pool_size, features // 2, features])
-            bd = self.__variable([features // 2], 0.0)
+            bd = self.__variable([features // 2], 0.1)
             h_deconv = tf.nn.relu(self.__deconv_layer(in_node, wd, pool_size) + bd)
             h_deconv_concat = self.__crop_and_concat(dw_h_convs[layer], h_deconv)
-            deconv[layer] = h_deconv_concat
 
             w1 = self.__weight_variable([filter_size, filter_size, features, features // 2])
             w2 = self.__weight_variable([filter_size, filter_size, features // 2, features // 2])
-            b1 = self.__variable([features // 2], 0.0)
-            b2 = self.__variable([features // 2], 0.0)
+            b1 = self.__variable([features // 2], 0.1)
+            b2 = self.__variable([features // 2], 0.1)
 
             conv1 = self.__conv_layer(h_deconv_concat, w1, self.keep_prob)
             h_conv = tf.nn.relu(conv1 + b1)
             conv2 = self.__conv_layer(h_conv, w2, self.keep_prob)
             in_node = tf.nn.relu(conv2 + b2)
-            up_h_convs[layer] = in_node
-
-            size *= 2
-            size -= 4
 
         # Output Map
         weight = self.__weight_variable([1, 1, features_root, 2])
-        bias = self.__variable([2], 0.0)
+        bias = self.__variable([2], 0.1)
         conv = self.__conv_layer(in_node, weight, tf.constant(1.0))
         output_map = tf.nn.relu(conv + bias)
-        up_h_convs["out"] = output_map
 
-        return output_map
+        logits = tf.reshape(output_map, (-1, 2))
 
-    def __adam_optimizer(self, learning_rate):
-        self.learning_rate_node = tf.Variable(learning_rate)
+        cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits,
+                                                                       labels=tf.reshape(expected, [-1]),
+                                                                       name='x_entropy')
 
-        optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate_node) \
-            .minimize(self.cost)
+        self.__loss = tf.reduce_mean(cross_entropy, name='x_entropy_mean')
 
-        return optimizer
+        self.__train_step = tf.train.AdamOptimizer(self.rate).minimize(self.__loss)
 
-    def __predict_function(self, logits):
-        flat_logits = tf.reshape(logits, (-1, 2))
-
-        return tf.argmax(tf.reshape(tf.nn.softmax(flat_logits), tf.shape(logits)), dimension=3)
-
-    def __cost_function(self, logits):
-        flat_logits = tf.reshape(logits, (-1, 2))
-        expected = tf.expand_dims(self.y, -1)
-
-        cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=flat_logits,
-                                                                       labels=tf.reshape(expected, [-1]))
-        loss = tf.reduce_mean(cross_entropy)
-
-        return loss
+        self.__prediction = tf.argmax(tf.reshape(tf.nn.softmax(logits), tf.shape(output_map)), dimension=3)
 
     def __weight_variable(self, shape):
         nr_units = functools.reduce(lambda x, y: x * y, shape)
@@ -221,27 +184,30 @@ class LVSegmentation(object):
         x1_crop = tf.slice(x1, offsets, size)
         return tf.concat([x1_crop, x2], 3)
 
-    def __output_epoch_stats(self, epoch, total_loss, training_iters):
-        print("Epoch {:}, Average loss: {:.4f}".format(epoch, (total_loss / training_iters)))
-
-    def __save(self, epoch):
-        self.saver.save(self.session, self.checkpoint_dir + 'model', global_step=epoch)
-
     def __restore_session(self):
-
-        if not os.path.exists(self.checkpoint_dir):
-            raise IOError(self.checkpoint_dir + ' does not exist.')
+        if not os.path.exists(self.__checkpoint_dir):
+            raise IOError(self.__checkpoint_dir + ' does not exist.')
         else:
-            path = tf.train.get_checkpoint_state(self.checkpoint_dir)
+            path = tf.train.get_checkpoint_state(self.__checkpoint_dir)
             if path is None:
-                raise IOError('No checkpoint to restore in ' + self.checkpoint_dir)
+                raise IOError('No checkpoint to restore in ' + self.__checkpoint_dir)
             else:
-                self.saver.restore(self.session, path.model_checkpoint_path)
+                self.__saver.restore(self.__session, path.model_checkpoint_path)
+
+        with open(self.__checkpoint_dir + 'loss.pickle', 'rb') as f:
+            self.__loss_array = pickle.load(f)
+
+    def __save_loss(self):
+        if os.path.exists(self.__checkpoint_dir + 'loss.pickle'):
+            os.remove(self.__checkpoint_dir + 'loss.pickle')
+
+        with open(self.__checkpoint_dir + 'loss.pickle', 'wb') as f:
+            pickle.dump(self.__loss_array, f)
 
 
 if __name__ == '__main__':
     train, val = sunnybrook.get_all_contours()
-    segmenter = LVSegmentation(1e-3)
+    segmenter = LVSegmentation()
 
     if len(sys.argv) != 2:
         print('The program must be run as : python3.5 step2_train_segmenter_v3.py [train|predict]')
@@ -250,7 +216,7 @@ if __name__ == '__main__':
         if sys.argv[1] == 'train':
             print('Run Train .....')
 
-            segmenter.train(train, 800, 2, epochs=30)
+            segmenter.train(train)
 
         elif sys.argv[1] == 'predict':
             print('Run Predict .....')
