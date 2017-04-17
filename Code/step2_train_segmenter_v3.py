8ae1c8064dafc166cd439c29c937c0f5eedb7588
Daniel194 <lungu.daniel94@gmail.com>
2017-04-17 14:32:22 +0300

    Segmenter 3 - U-Net - Redesign

--- Code/step2_train_segmenter_v3.py
+++ Code/step2_train_segmenter_v3.py
@@ -7,13 +7,12 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 import functools
-from collections import OrderedDict
 import utils.sunnybrook as sunnybrook
 
 
 class LVSegmentation(object):
     def __init__(self, checkpoint_dir='../../result/segmenter/train_result/v3/'):
-        self.__create_conv_net()
+        self.__build_con_net()
         self.__saver = tf.train.Saver(max_to_keep=30, keep_checkpoint_every_n_hours=1)
         self.__session = tf.Session()
         self.__session.run(tf.global_variables_initializer())
@@ -74,8 +73,7 @@ class LVSegmentation(object):
 
         return before_normalization, images, labels
 
-    def __create_conv_net(self, layers=5, features_root=32, filter_size=3, pool_size=2):
-
+    def __build_con_net(self):
         self.x = tf.placeholder(tf.float32, shape=(None, 224, 224, 1))
         self.y = tf.placeholder(tf.int64, shape=(None, 224, 224))
         self.keep_prob = tf.placeholder(tf.float32)
@@ -83,60 +81,60 @@ class LVSegmentation(object):
 
         expected = tf.expand_dims(self.y, -1)
 
-        dw_h_convs = OrderedDict()
-        in_node = self.x
+        conv_1_1 = self.__conv_layer(self.x, [3, 3, 1, 32], 32, self.keep_prob)
+        conv_1_2 = self.__conv_layer(conv_1_1, [3, 3, 32, 32], 32, self.keep_prob)
 
-        # down layers
-        for layer in range(0, layers):
-            features = 2 ** layer * features_root
+        pool_1 = self.__max_pool(conv_1_2)
 
-            if layer == 0:
-                w1 = self.__weight_variable([filter_size, filter_size, 1, features])
-            else:
-                w1 = self.__weight_variable([filter_size, filter_size, features // 2, features])
+        conv_2_1 = self.__conv_layer(pool_1, [3, 3, 32, 64], 64, self.keep_prob)
+        conv_2_2 = self.__conv_layer(conv_2_1, [3, 3, 64, 64], 64, self.keep_prob)
 
-            w2 = self.__weight_variable([filter_size, filter_size, features, features])
+        pool_2 = self.__max_pool(conv_2_2)
 
-            b1 = self.__variable([features], 0.1)
-            b2 = self.__variable([features], 0.1)
+        conv_3_1 = self.__conv_layer(pool_2, [3, 3, 64, 128], 128, self.keep_prob)
+        conv_3_2 = self.__conv_layer(conv_3_1, [3, 3, 128, 128], 128, self.keep_prob)
 
-            conv1 = self.__conv_layer(in_node, w1, self.keep_prob)
-            tmp_h_conv = tf.nn.relu(conv1 + b1)
-            conv2 = self.__conv_layer(tmp_h_conv, w2, self.keep_prob)
-            dw_h_convs[layer] = tf.nn.relu(conv2 + b2)
+        pool_3 = self.__max_pool(conv_3_2)
 
-            if layer < layers - 1:
-                in_node = self.__max_pool(dw_h_convs[layer], pool_size)
+        conv_4_1 = self.__conv_layer(pool_3, [3, 3, 128, 256], 256, self.keep_prob)
+        conv_4_2 = self.__conv_layer(conv_4_1, [3, 3, 256, 256], 256, self.keep_prob)
 
-        in_node = dw_h_convs[layers - 1]
+        pool_4 = self.__max_pool(conv_4_2)
 
-        # up layers
-        for layer in range(layers - 2, -1, -1):
-            features = 2 ** (layer + 1) * features_root
+        conv_5_1 = self.__conv_layer(pool_4, [3, 3, 256, 512], 512, self.keep_prob)
+        conv_5_2 = self.__conv_layer(conv_5_1, [3, 3, 512, 512], 512, self.keep_prob)
 
-            wd = self.__weight_variable([pool_size, pool_size, features // 2, features])
-            bd = self.__variable([features // 2], 0.1)
-            h_deconv = tf.nn.relu(self.__deconv_layer(in_node, wd, pool_size) + bd)
-            h_deconv_concat = self.__crop_and_concat(dw_h_convs[layer], h_deconv)
+        deconv_4 = self.__deconv_layer(conv_5_2, [2, 2, 256, 512], 256)
 
-            w1 = self.__weight_variable([filter_size, filter_size, features, features // 2])
-            w2 = self.__weight_variable([filter_size, filter_size, features // 2, features // 2])
-            b1 = self.__variable([features // 2], 0.1)
-            b2 = self.__variable([features // 2], 0.1)
+        deconv_4_concat = self.__crop_and_concat(conv_4_2, deconv_4)
 
-            conv1 = self.__conv_layer(h_deconv_concat, w1, self.keep_prob)
-            h_conv = tf.nn.relu(conv1 + b1)
-            conv2 = self.__conv_layer(h_conv, w2, self.keep_prob)
-            in_node = tf.nn.relu(conv2 + b2)
+        conv_6_1 = self.__conv_layer(deconv_4_concat, [3, 3, 512, 256], 256, self.keep_prob)
+        conv_6_2 = self.__conv_layer(conv_6_1, [3, 3, 256, 256], 256, self.keep_prob)
 
-        # Output Map
-        weight = self.__weight_variable([1, 1, features_root, 2])
-        bias = self.__variable([2], 0.1)
-        conv = self.__conv_layer(in_node, weight, tf.constant(1.0))
-        output_map = tf.nn.relu(conv + bias)
+        deconv_3 = self.__deconv_layer(conv_6_2, [2, 2, 128, 256], 128)
 
-        logits = tf.reshape(output_map, (-1, 2))
+        deconv_3_concat = self.__crop_and_concat(conv_3_2, deconv_3)
 
+        conv_7_1 = self.__conv_layer(deconv_3_concat, [3, 3, 256, 128], 128, self.keep_prob)
+        conv_7_2 = self.__conv_layer(conv_7_1, [3, 3, 128, 128], 128, self.keep_prob)
+
+        deconv_2 = self.__deconv_layer(conv_7_2, [2, 2, 64, 128], 64)
+
+        deconv_2_concat = self.__crop_and_concat(conv_2_2, deconv_2)
+
+        conv_8_1 = self.__conv_layer(deconv_2_concat, [3, 3, 128, 64], 64, self.keep_prob)
+        conv_8_2 = self.__conv_layer(conv_8_1, [3, 3, 64, 64], 64, self.keep_prob)
+
+        deconv_1 = self.__deconv_layer(conv_8_2, [2, 2, 32, 64], 32)
+
+        deconv_1_concat = self.__crop_and_concat(conv_1_2, deconv_1)
+
+        conv_9_1 = self.__conv_layer(deconv_1_concat, [3, 3, 64, 32], 32, self.keep_prob)
+        conv_9_2 = self.__conv_layer(conv_9_1, [3, 3, 32, 32], 32, self.keep_prob)
+
+        output = self.__conv_layer(conv_9_2, [1, 1, 32, 2], 2, tf.constant(1.0))
+
+        logits = tf.reshape(output, (-1, 2))
         cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits,
                                                                        labels=tf.reshape(expected, [-1]),
                                                                        name='x_entropy')
@@ -145,7 +143,7 @@ class LVSegmentation(object):
 
         self.__train_step = tf.train.AdamOptimizer(self.rate).minimize(self.__loss)
 
-        self.__prediction = tf.argmax(tf.reshape(tf.nn.softmax(logits), tf.shape(output_map)), dimension=3)
+        self.__prediction = tf.argmax(tf.reshape(tf.nn.softmax(logits), tf.shape(output)), dimension=3)
 
     def __weight_variable(self, shape):
         nr_units = functools.reduce(lambda x, y: x * y, shape)
@@ -155,25 +153,39 @@ class LVSegmentation(object):
 
         return tf.Variable(initial)
 
-    def __variable(self, shape, constant):
-        initial = tf.constant(constant, shape=shape)
+    def __bias_variable(self, shape):
+        initial = tf.constant(0.1, shape=shape)
 
         return tf.Variable(initial)
 
-    def __conv_layer(self, x, weights, keep_prob_):
+    def __conv_layer(self, x, w_shape, b_shape, keep_prob):
+
+        weights = self.__weight_variable(w_shape)
+        biases = self.__bias_variable([b_shape])
+
         hidden = tf.nn.conv2d(x, weights, strides=[1, 1, 1, 1], padding='SAME')
+        hidden = tf.add(hidden, biases)
+        hidden = tf.nn.relu(hidden)
+        hidden = tf.nn.dropout(hidden, keep_prob)
 
-        return tf.nn.dropout(hidden, keep_prob_)
+        return hidden
 
-    def __deconv_layer(self, x, weights, stride):
+    def __deconv_layer(self, x, w_shape, b_shape):
+
+        weights = self.__weight_variable(w_shape)
+        biases = self.__bias_variable([b_shape])
 
         x_shape = tf.shape(x)
         out_shape = tf.stack([x_shape[0], x_shape[1] * 2, x_shape[2] * 2, x_shape[3] // 2])
 
-        return tf.nn.conv2d_transpose(x, weights, out_shape, strides=[1, stride, stride, 1], padding='SAME')
+        hidden = tf.nn.conv2d_transpose(x, weights, out_shape, strides=[1, 2, 2, 1], padding='SAME')
+        hidden = tf.add(hidden, biases)
+        hidden = tf.nn.relu(hidden)
 
-    def __max_pool(self, x, n):
-        return tf.nn.max_pool(x, ksize=[1, n, n, 1], strides=[1, n, n, 1], padding='SAME')
+        return hidden
+
+    def __max_pool(self, x):
+        return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
     def __crop_and_concat(self, x1, x2):
         x1_shape = tf.shape(x1)
