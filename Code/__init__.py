ea9572c548b9f7a1884f9a35b231fd110aea6377
Daniel194 <lungu.daniel94@gmail.com>
2017-06-27 00:49:35 +0300

    Demo - step 1 - Done

--- Code/__init__.py
+++ Code/__init__.py
@@ -1,17 +1,12 @@
 import step1_preprocess as preprocess
-import utils.sunnybrook as sunnybrook
-import numpy as np
 
-##### STEP 1 - Preprocess #####
+########## STEP 1 - Preprocess ##########
 
 preprocess.convert_sax_images(rescale=True, base_size=256, crop_size=256)
-# preprocess.create_csv_data()
-# preprocess.enrich_dicom_csvdata()
-# preprocess.enrich_traindata()
-#
-# # Convert Sunnybrook dataset from DICOM form to PNG format
-# train, val = sunnybrook.get_all_contours()
-# ctrs = np.append(train, val)
-# sunnybrook.convert_dicom_to_png(ctrs)
+preprocess.create_csv_data()
+preprocess.enrich_dicom_csvdata()
+preprocess.enrich_traindata()
 
 print("Done - Step 1")
+
+########## STEP 2 - Segmentation ##########
